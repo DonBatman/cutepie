484000f7d0e5dad651443b6baaec2bae040a020f
DonBatman <don@serfdon.com>
2016-03-29 17:57:24 -0700

    Code cleanup

--- crazyblocks.lua
+++ crazyblocks.lua
@@ -1,93 +1,25 @@
+local crazy_blocks = {
+	{"cute_pinknblue","Cutepie Pink n Blue"},
+	{"cute_greenx","Cutepie Green X"},
+	{"cute_greennorange","Cutepie Green n Orange"},
+	{"cute_bluex","Cutepie Blue X"},
+	{"cute_pink","Cutepie Pink"},
+	{"cute_blue","Cutepie Blue"},
+	{"cute_orange","Cutepie Orange"},
+	{"cute_green","Cutepie Green"}
+	}
 
+for i in ipairs(crazy_blocks) do
+	local itm = crazy_blocks[i][1]
+	local des = crazy_blocks[i][2]
 
--- cute pinknblue
-
-minetest.register_node("cutepie:cute_pinknblue", {
-	description = "Cutepie Pink n Blue",
+minetest.register_node("cutepie:"..itm, {
+	description = des,
 	drawtype = "normal",
 	paramtype = "light",
-	tiles = {"cute_pinknblue.png"},
+	tiles = {itm..".png"},
 	paramtype = "light",
 	groups = {cracky = 2},
 
 })
-
--- cute greenx
-
-minetest.register_node("cutepie:cute_greenx", {
-	description = "Cutepie Green X",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_greenx.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
-
--- cute greennorange
-
-minetest.register_node("cutepie:cute_greennorange", {
-	description = "Cutepie Green n Orange",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_greennorange.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
-
--- cute bluex
-
-minetest.register_node("cutepie:cute_bluex", {
-	description = "Cutepie Blue X",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_bluex.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
--- cute pink
-
-minetest.register_node("cutepie:cute_pink", {
-	description = "Cutepie Pink",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_pink.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
--- cute blue
-
-minetest.register_node("cutepie:cute_blue", {
-	description = "Cutepie Blue",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_blue.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
--- cute orange
-
-minetest.register_node("cutepie:cute_orange", {
-	description = "Cutepie Orange",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_orange.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
--- cute green
-
-minetest.register_node("cutepie:cute_green", {
-	description = "Cutepie Green",
-	drawtype = "normal",
-	paramtype = "light",
-	tiles = {"cute_green.png"},
-	paramtype = "light",
-	groups = {cracky = 2},
-
-})
+end
